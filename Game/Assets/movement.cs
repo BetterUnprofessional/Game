fb4745219e0acd0fb1e41d6d9b6f0df6df198666
BetterUnprofessional <105175991+BetterUnprofessional@users.noreply.github.com>
2023-05-29 17:57:46 -0400

    Fixed shit

--- Game/Assets/movement.cs
+++ Game/Assets/movement.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
-{
-
-    Vector3 Move;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Move = new Vector3(5, 6, 5);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)){
-             Debug.Log("Space");
-            //Spacebar was pressed
-             transform.Translate(Move.z * Time.deltaTime);
-             Debug.Log(transform.position.y);
-            
-        }
-    }
-}
+ 
+ public class example : MonoBehaviour {
+     //Variables
+     public float speed = 6.0F;
+     public float jumpSpeed = 8.0F; 
+     public float gravity = 20.0F;
+     private Vector3 moveDirection = Vector3.zero;
+ 
+     void Update() {
+         CharacterController controller = GetComponent<CharacterController>();
+         // is the controller on the ground?
+         if (controller.isGrounded) {
+             //Feed moveDirection with input.
+             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+             moveDirection = transform.TransformDirection(moveDirection);
+             //Multiply it by speed.
+             moveDirection *= speed;
+             //Jumping
+             if (Input.GetButton("Jump"))
+                 moveDirection.y = jumpSpeed;
+             
+         }
+         //Applying gravity to the controller
+         moveDirection.y -= gravity * Time.deltaTime;
+         //Making the character move
+         controller.Move(moveDirection * Time.deltaTime);
+     }
+ }
