8c96797102275b2a6f21d26f305009cf9a942b00
Nautilotl <sfmarsano@gmail.com>
2023-05-31 17:49:44 -0400

    w fe

--- Game/Assets/rigi.cs
+++ Game/Assets/rigi.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class rigi : MonoBehaviour
 {
-
-
-    
     // Start is called before the first frame update
     void Start()
     {
